51d0786d5017d0a12b8783cbe9a572611d0d0158
Stanislas Rigal <stanislas.rigal@laposte.net>
2024-01-24 18:21:22 +0100

    pressure data

--- bird_data.R
+++ bird_data.R
@@ -10,6 +10,12 @@ sites <- read.table(file = "raw_data/pecbms_bird_data/sites.txt", header = TRUE,
 
 sample_sites <- sites[which(sites$siteID %in% unique(bird_data$siteID)),] # idem sites
 
+sample_sites_line <- sample_sites[which(sample_sites$method=="Line transect"),]
+
+sample_sites_point <- sample_sites[which(sample_sites$method=="Point counts"),]
+
+sample_sites_territory <- sample_sites[which(sample_sites$method=="Territory mapping"),]
+
 sf::sf_use_s2(FALSE)
 
 worldmap <- ne_countries(scale = 'medium', type = 'countries',returnclass = 'sf')
@@ -40,4 +46,38 @@ europe_map_simpl <- ms_simplify(europe_cropped, keep = 0.5,
 ggplot() + geom_sf(data = europe_map_simpl, aes(fill = fill_param)) + scale_fill_discrete(na.value="lightgrey")+
   theme_void()+ coord_sf(datum = NA) + geom_point(data=sample_sites, aes(x=Long_WGS84, y=Lat_WGS84),size=0.5)
 
+ggplot() + geom_sf(data = europe_map_simpl, aes(fill = fill_param)) + scale_fill_discrete(na.value="lightgrey")+
+  theme_void()+ coord_sf(datum = NA) + geom_point(data=sample_sites_line, aes(x=Long_WGS84, y=Lat_WGS84),size=0.5)
+
+ggplot() + geom_sf(data = europe_map_simpl, aes(fill = fill_param)) + scale_fill_discrete(na.value="lightgrey")+
+  theme_void()+ coord_sf(datum = NA) + geom_point(data=sample_sites_point, aes(x=Long_WGS84, y=Lat_WGS84),size=0.5)
+
+ggplot() + geom_sf(data = europe_map_simpl, aes(fill = fill_param)) + scale_fill_discrete(na.value="lightgrey")+
+  theme_void()+ coord_sf(datum = NA) + geom_point(data=sample_sites_territory, aes(x=Long_WGS84, y=Lat_WGS84),size=0.5)
+
+
+# country example
+
+fr_cropped <- st_crop(worldmap[worldmap$sovereignt %in% c("France"),],
+                          xmin = -12, xmax = 35,ymin = 30, ymax = 73)
+
+fr_cropped$fill_param <- rep("a",nrow(fr_cropped))
+
+ggplot() + geom_sf(data = fr_cropped, aes(fill = fill_param)) + scale_fill_discrete(na.value="lightgrey")+
+  theme_void()+ coord_sf(datum = NA) + geom_point(data=sample_sites[which(sample_sites$scheme_code=="FR"),], aes(x=Long_WGS84, y=Lat_WGS84),size=0.5)
+
+ge_cropped <- st_crop(worldmap[worldmap$sovereignt %in% c("Germany"),],
+                      xmin = -12, xmax = 35,ymin = 30, ymax = 73)
+
+ge_cropped$fill_param <- rep("a",nrow(fr_cropped))
+
+ggplot() + geom_sf(data = ge_cropped, aes(fill = fill_param)) + scale_fill_discrete(na.value="lightgrey")+
+  theme_void()+ coord_sf(datum = NA) + geom_point(data=sample_sites[which(sample_sites$scheme_code=="DE"),], aes(x=Long_WGS84, y=Lat_WGS84),size=0.5)
+
+
+# Distribution type sampling
+
+ggplot() +
+  geom_density(data=sample_sites, aes(x=area_sampled_m2, col=method)) +
+  xlim(0,5000000) + theme_modern()
 
--- landuse.R
+++ landuse.R
@@ -1,3 +1,178 @@
 # load land use tile for example from https://doi.org/10.7910/DVN/86M4PO and https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/86M4PO
 
 ex_tile <- raster(x = "raw_data/landuse_intensity/Cropping_Intensity_30m_2016_2018_N50E000.tif")
+
+
+# european land system 
+
+eu_land_system <- raster(x = "raw_data/land_system/EU_landSystem.tif")
+
+
+# protected areas 
+#UNEP-WCMC and IUCN (2024), Protected Planet: The World Database on Protected Areas (WDPA) and World Database on Other Effective Area-based Conservation Measures (WD-OECM) [Online], January 2024, Cambridge, UK: UNEP-WCMC and IUCN. Available at: www.protectedplanet.net.
+
+protected_area_shp0 <- read_sf(dsn = "raw_data/protected_area/WDPA_shp_0/", layer = "WDPA_WDOECM_Jan2024_Public_EU_shp-polygons")
+protected_area_shp1 <- read_sf(dsn = "raw_data/protected_area/WDPA_shp_1/", layer = "WDPA_WDOECM_Jan2024_Public_EU_shp-polygons")
+protected_area_shp2 <- read_sf(dsn = "raw_data/protected_area/WDPA_shp_2/", layer = "WDPA_WDOECM_Jan2024_Public_EU_shp-polygons")
+protected_area <- rbind(protected_area_shp0, protected_area_shp1)
+protected_area <- rbind(protected_area, protected_area_shp2)
+
+# pesticide
+
+pesticide_table <- read.csv("raw_data/pesticide/pesticideActiveSubtances.csv")
+pesticide_table <- pesticide_table[which(!(pesticide_table$COUNTRY %in% c("1-","2-","3-","4-","5-","6-"))),]
+
+### from quantity to unit dose
+
+pesticide_dose <- read.csv2("raw_data/pesticide/pesticide_fr/agence_eau/usage_pesticide.csv", header=TRUE)
+
+grep("BOSCALID",unique(pesticide_dose$sa),ignore.case=TRUE,value=TRUE)
+
+grep("BOSCALID",unique(pesticide_dose$sa),ignore.case=TRUE,value=TRUE)[min(length(grep("BOSCALID",unique(pesticide_dose$sa),ignore.case=TRUE,value=TRUE)))]
+
+
+pesticide_table <- as.data.table(pesticide_table %>% group_by(COUNTRY,NUTS3,Categories_of_products) %>% summarize(total=sum(KG_TOT)))
+pesticide_table <- dcast(pesticide_table,COUNTRY + NUTS3 ~ Categories_of_products, value.var="total")
+pesticide_table$total <- apply(pesticide_table[,3:ncol(pesticide_table)],1,sum)
+pesticide_table$id <- pesticide_table$NUTS3
+
+### update NUTS names
+pesticide_table$id <- sub("FR21","FRF2",pesticide_table$id)
+pesticide_table$id <- sub("FR22","FRE2",pesticide_table$id)
+pesticide_table$id <- sub("FR23","FRD2",pesticide_table$id)
+pesticide_table$id <- sub("FR24","FRB0",pesticide_table$id)
+pesticide_table$id <- sub("FR25","FRD1",pesticide_table$id)
+pesticide_table$id <- sub("FR26","FRC1",pesticide_table$id)
+pesticide_table$id <- sub("FR30","FRE1",pesticide_table$id)
+pesticide_table$id <- sub("FR41","FRF3",pesticide_table$id)
+pesticide_table$id <- sub("FR42","FRF1",pesticide_table$id)
+pesticide_table$id <- sub("FR43","FRC2",pesticide_table$id)
+pesticide_table$id <- sub("FR51","FRG0",pesticide_table$id)
+pesticide_table$id <- sub("FR52","FRH0",pesticide_table$id)
+pesticide_table$id <- sub("FR53","FRI3",pesticide_table$id)
+pesticide_table$id <- sub("FR61","FRI1",pesticide_table$id)
+pesticide_table$id <- sub("FR62","FRJ2",pesticide_table$id)
+pesticide_table$id <- sub("FR63","FRI2",pesticide_table$id)
+pesticide_table$id <- sub("FR71","FRK2",pesticide_table$id)
+pesticide_table$id <- sub("FR72","FRK1",pesticide_table$id)
+pesticide_table$id <- sub("FR81","FRJ1",pesticide_table$id)
+pesticide_table$id <- sub("FR82","FRL0",pesticide_table$id)
+pesticide_table$id <- sub("FR83","FRM0",pesticide_table$id)
+#pesticide_table$id <- sub("FRA","FRY",pesticide_table$id)
+pesticide_table$id <- sub("DE915","DE91C",pesticide_table$id)
+pesticide_table$id <- sub("DE919","DE91C",pesticide_table$id)
+pesticide_table$id <- sub("DEB16","DEB1C",pesticide_table$id)
+pesticide_table$id <- sub("DEB19","DEB1D",pesticide_table$id)
+pesticide_table$id <- sub("FI1D4","FI1D8",pesticide_table$id)
+pesticide_table$id <- sub("FI1D6","FI1D9",pesticide_table$id)
+pesticide_table$id <- sub("HU101","HU110",pesticide_table$id)
+pesticide_table$id <- sub("HU102","HU120",pesticide_table$id)
+pesticide_table$id <- sub("IE011","IE041",pesticide_table$id)
+pesticide_table$id <- sub("IE012","IE063",pesticide_table$id)
+pesticide_table$id <- sub("IE013","IE042",pesticide_table$id)
+pesticide_table$id <- sub("IE021","IE061",pesticide_table$id)
+pesticide_table$id <- sub("IE022","IE062",pesticide_table$id)
+pesticide_table$id <- sub("IE023","IE051",pesticide_table$id)
+pesticide_table$id <- sub("IE024","IE052",pesticide_table$id)
+pesticide_table$id <- sub("IE025","IE053",pesticide_table$id)
+pesticide_table$id <- sub("LT00A","LT011",pesticide_table$id)
+pesticide_table$id <- sub("LT001","LT021",pesticide_table$id)
+pesticide_table$id <- sub("LT002","LT022",pesticide_table$id)
+pesticide_table$id <- sub("LT003","LT023",pesticide_table$id)
+pesticide_table$id <- sub("LT004","LT024",pesticide_table$id)
+pesticide_table$id <- sub("LT005","LT025",pesticide_table$id)
+pesticide_table$id <- sub("LT006","LT026",pesticide_table$id)
+pesticide_table$id <- sub("LT007","LT027",pesticide_table$id)
+pesticide_table$id <- sub("LT008","LT028",pesticide_table$id)
+pesticide_table$id <- sub("LT009","LT029",pesticide_table$id)
+pesticide_table$id <- sub("NL121","NL124",pesticide_table$id)
+pesticide_table$id <- sub("NL122","NL125",pesticide_table$id)
+pesticide_table$id <- sub("NL123","NL126",pesticide_table$id)
+pesticide_table$id <- sub("NL322","NL328",pesticide_table$id)
+pesticide_table$id <- sub("NL326","NL329",pesticide_table$id)
+pesticide_table$id <- sub("NL338","NL33B",pesticide_table$id)
+pesticide_table$id <- sub("NL339","NL33C",pesticide_table$id)
+pesticide_table$id <- sub("NO061","NO060",pesticide_table$id)
+pesticide_table$id <- sub("NO062","NO060",pesticide_table$id)
+pesticide_table$id <- sub("PL113","PL711",pesticide_table$id)
+pesticide_table$id <- sub("PL114","PL712",pesticide_table$id)
+pesticide_table$id <- sub("PL115","PL713",pesticide_table$id)
+pesticide_table$id <- sub("PL116","PL714",pesticide_table$id)
+pesticide_table$id <- sub("PL117","PL715",pesticide_table$id)
+pesticide_table$id <- sub("PL127","PL911",pesticide_table$id)
+pesticide_table$id <- sub("PL128","PL921",pesticide_table$id)
+pesticide_table$id <- sub("PL129","PL912",pesticide_table$id)
+pesticide_table$id <- sub("PL12A","PL913",pesticide_table$id)
+pesticide_table$id <- sub("PL12B","PL922",pesticide_table$id)
+pesticide_table$id <- sub("PL12C","PL923",pesticide_table$id)
+pesticide_table$id <- sub("PL12D","PL924",pesticide_table$id)
+pesticide_table$id <- sub("PL12E","PL925",pesticide_table$id)
+pesticide_table$id <- sub("PL311","PL811",pesticide_table$id)
+pesticide_table$id <- sub("PL312","PL812",pesticide_table$id)
+pesticide_table$id <- sub("PL314","PL814",pesticide_table$id)
+pesticide_table$id <- sub("PL315","PL815",pesticide_table$id)
+pesticide_table$id <- sub("PL323","PL821",pesticide_table$id)
+pesticide_table$id <- sub("PL324","PL822",pesticide_table$id)
+pesticide_table$id <- sub("PL325","PL823",pesticide_table$id)
+pesticide_table$id <- sub("PL326","PL824",pesticide_table$id)
+pesticide_table$id <- sub("PL331","PL721",pesticide_table$id)
+pesticide_table$id <- sub("PL332","PL722",pesticide_table$id)
+pesticide_table$id <- sub("PL343","PL841",pesticide_table$id)
+pesticide_table$id <- sub("PL344","PL842",pesticide_table$id)
+pesticide_table$id <- sub("PL345","PL843",pesticide_table$id)
+temporary_row <- pesticide_table[which(pesticide_table$id=="PL913"),]
+temporary_row$id <- "PL926"
+pesticide_table <- rbind(pesticide_table,temporary_row)
+pesticide_table$id <- sub("UKM21","UKM71",pesticide_table$id)
+pesticide_table$id <- sub("UKM22","UKM72",pesticide_table$id)
+pesticide_table$id <- sub("UKM23","UKM73",pesticide_table$id)
+pesticide_table$id <- sub("UKM24","UKM91",pesticide_table$id)
+pesticide_table$id <- sub("UKM25","UKM75",pesticide_table$id)
+pesticide_table$id <- sub("UKM26","UKM76",pesticide_table$id)
+pesticide_table$id <- sub("UKM27","UKM77",pesticide_table$id)
+pesticide_table$id <- sub("UKM28","UKM78",pesticide_table$id)
+pesticide_table$id <- sub("UKM31","UKM81",pesticide_table$id)
+pesticide_table$id <- sub("UKM32","UKM92",pesticide_table$id)
+pesticide_table$id <- sub("UKM33","UKM93",pesticide_table$id)
+pesticide_table$id <- sub("UKM34","UKM82",pesticide_table$id)
+pesticide_table$id <- sub("UKM35","UKM83",pesticide_table$id)
+pesticide_table$id <- sub("UKM36","UKM84",pesticide_table$id)
+pesticide_table$id <- sub("UKM37","UKM94",pesticide_table$id)
+pesticide_table$id <- sub("UKM38","UKM95",pesticide_table$id)
+pesticide_table$id <- sub("UKN01","UKN06",pesticide_table$id)
+pesticide_table$id <- sub("UKN02","UKN14",pesticide_table$id)
+pesticide_table$id <- sub("UKN03","UKN13",pesticide_table$id) # UKN15 UKN09
+temporary_row <- pesticide_table[which(pesticide_table$id=="UKN13"),]
+temporary_row$id <- "UKN15"
+pesticide_table <- rbind(pesticide_table,temporary_row)
+temporary_row <- pesticide_table[which(pesticide_table$id=="UKN13"),]
+temporary_row$id <- "UKN09"
+pesticide_table <- rbind(pesticide_table,temporary_row)
+pesticide_table$id <- sub("UKN04","UKN10",pesticide_table$id) # UKN12
+temporary_row <- pesticide_table[which(pesticide_table$id=="UKN10"),]
+temporary_row$id <- "UKN12"
+pesticide_table <- rbind(pesticide_table,temporary_row)
+pesticide_table$id <- sub("UKN05","UKN16",pesticide_table$id) # UKN11 UKN08 UKN07
+temporary_row <- pesticide_table[which(pesticide_table$id=="UKN16"),]
+temporary_row$id <- "UKN11"
+pesticide_table <- rbind(pesticide_table,temporary_row)
+temporary_row <- pesticide_table[which(pesticide_table$id=="UKN16"),]
+temporary_row$id <- "UKN08"
+pesticide_table <- rbind(pesticide_table,temporary_row)
+temporary_row <- pesticide_table[which(pesticide_table$id=="UKN16"),]
+temporary_row$id <- "UKN07"
+pesticide_table <- rbind(pesticide_table,temporary_row)
+
+### download eurostat data of population counts by NUTS-3 region
+#euro_pop <- get_eurostat('demo_r_pjanaggr3', stringsAsFactors = FALSE) %>%  filter(sex == 'T', str_length(geo) == 5, age == 'TOTAL') # NUTS-3
+
+### download geospatial data for NUTS-3 regions
+
+euro_nuts3_sf <- get_eurostat_geospatial(output_class = 'sf', 
+                                         resolution ='60', nuts_level = "3") %>%
+  st_transform(crs = 3035)
+
+pesticide_sf <- merge(euro_nuts3_sf,pesticide_table)
+
+ggplot(pesticide_sf) +
+  geom_sf(aes(fill = total))
