5aa4164d8737aaa92b555f2e6f8cecdc0bb32bdc
Stanislas Rigal <stanislas.rigal@laposte.net>
2024-07-12 17:20:51 +0200

    Prepare bird_data and pressure for species analysis.

--- analysis_bird.R
+++ analysis_bird.R
@@ -19,7 +19,7 @@ grid_eu_cat <- grid_eu_all[which(grid_eu_all$NUTS2021_2 == "ES511"),]
 st_write(grid_eu_cat,"output/grid_eu_cat_test.gpkg")
 grid_eu_cat <- st_read("output/grid_eu_cat_test.gpkg")
 
-# site and bird data from the Catalonian survey
+## site and bird data from the Catalonian survey
 
 sites <- read.table(file = "raw_data/pecbms_bird_data/sites.txt", header = TRUE, sep = "\t")
 
@@ -33,7 +33,7 @@ st_crs(site_cat_sf) <- 4326
 
 site_cat_sf_reproj <- st_transform(site_cat_sf,crs(grid_eu_cat))
 
-# site and bird data from the Spanish survey in Catalonia
+## site and bird data from the Spanish survey in Catalonia
 
 bird_data_esp <- bird_data_clean[which(bird_data_clean$scheme_code %in% c("ES")),]
 
@@ -46,7 +46,7 @@ site_esp_sf_reproj <- st_transform(site_esp_sf,crs(grid_eu_cat))
 
 site_esp_sf_reproj <- st_intersection(site_esp_sf_reproj,grid_eu_cat)
 
-# merge both and get bird data
+## merge both and get bird data
 
 site_cat_sf_all <- rbind(site_cat_sf_reproj, site_esp_sf_reproj[,c(1:(ncol(site_cat_sf_reproj)-1),ncol(site_esp_sf_reproj))])
 
@@ -54,6 +54,7 @@ bird_data_cat_all <- bird_data_clean[which(bird_data_clean$siteID %in% unique(si
 
 site_cat_all <- sites[which(sites$siteID %in% unique(bird_data_cat_all$siteID)),]
 
+## plot sites and area monitored
 
 ggplot(grid_eu_cat) +
   geom_sf() +
@@ -71,6 +72,109 @@ ggplot(area_site_cat) +
   geom_sf() 
 
 area_site_cat$area <- as.numeric(st_area(area_site_cat))
+
+## plot a zoom
+
+crop_factor <- st_bbox(c(xmin = 1.5, 
+                         xmax = 2.5, 
+                         ymax = 41, 
+                         ymin = 42),
+                       crs = st_crs(site_esp_sf))
+
+grid_eu_cat_cropped <- st_transform(grid_eu_cat,st_crs(crop_factor))
+grid_eu_cat_cropped <- st_crop(grid_eu_cat_cropped, crop_factor)
+site_cat_sf_all_cropped <- st_transform(site_cat_sf_all,st_crs(crop_factor))
+site_cat_sf_all_cropped <- st_crop(site_cat_sf_all_cropped, crop_factor)
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf() +
+  geom_sf(data=site_cat_sf_all_cropped)
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=impervious2006)) +
+  scale_fill_viridis_b()
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=treedensity2012)) +
+  scale_fill_viridis_b()
+
+grid_eu_cat_cropped$eulandsystem_cat <- NA
+grid_eu_cat_cropped$eulandsystem_cat[which(grid_eu_cat_cropped$eulandsystem %in% c(11,12,13,80,90))] <-"no_intensity"
+grid_eu_cat_cropped$eulandsystem_cat[which(grid_eu_cat_cropped$eulandsystem %in% c(21,31,41,51,61,731,71:75))] <-"low_intensity"
+grid_eu_cat_cropped$eulandsystem_cat[which(grid_eu_cat_cropped$eulandsystem %in% c(22,32,42,52,62,732))] <-"medium_intensity"
+grid_eu_cat_cropped$eulandsystem_cat[which(grid_eu_cat_cropped$eulandsystem %in% c(23,43,53,63,733))] <-"high_intensity"
+grid_eu_cat_cropped$eulandsystem_cat <- factor(grid_eu_cat_cropped$eulandsystem_cat, levels = c("no_intensity","low_intensity","medium_intensity","high_intensity")) 
+grid_eu_cat_cropped$eulandsystem <- NULL
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=eulandsystem_cat)) +
+  scale_fill_viridis_d()
+
+ggsave(
+  "output/euland.png",
+  width = 5,
+  height = 5,
+  dpi = 300,
+)
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=lightpollution2000)) +
+  scale_fill_viridis_b()
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=pesticide_nodu_kg)) +
+  scale_fill_viridis_b()
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=tempspring2000)) +
+  scale_fill_viridis_b()
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=precspring2000)) +
+  scale_fill_viridis_b()
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=shannon)) +
+  scale_fill_viridis_b()
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=GDP2015)) +
+  scale_fill_viridis_b()
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=pop2020)) +
+  scale_fill_viridis_b()
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=protectedarea)) +
+  scale_fill_viridis_b()
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=woodprodaverage)) +
+  scale_fill_viridis_b()
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=drymatter2018)) +
+  scale_fill_viridis_b()
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=smallwoodyfeatures)) +
+  scale_fill_viridis_b()
+
+ggplot(grid_eu_cat_cropped) +
+  geom_sf(aes(fill=fragmentation)) +
+  scale_fill_viridis_b()
+
+area_site_cat_cropped <- st_transform(area_site_cat,st_crs(crop_factor))
+area_site_cat_cropped <- st_crop(area_site_cat_cropped, crop_factor)
+
+ggplot() +
+  geom_sf(data =area_site_cat_cropped)
+
+
+
+
+## summarize external variable for each site and format them
 
 value_site_cat <- as.data.frame(area_site_cat) %>% group_by(siteID) %>% summarize(pop2000 = weighted.mean(pop2000,area), pop2020 = weighted.mean(pop2020,area),
                                                                                   impervious2006 = weighted.mean(impervious2006,area), impervious2018 = weighted.mean(impervious2018,area),
@@ -102,7 +206,18 @@ pop2015 <- (value_site_cat$pop2020-value_site_cat$pop2000)/20*2015+value_site_ca
 value_site_cat$GDP2015_percap <- value_site_cat$GDP2015/pop2015
 value_site_cat$diff_gdp_percap <- (value_site_cat$GDP2015_percap-value_site_cat$GDP2000_percap)/value_site_cat$GDP2000_percap
 
-# add zero when species no present at monitored site
+value_site_cat$protectedarea[which(is.na(value_site_cat$protectedarea))] <- 0
+value_site_cat$protectedarea[which(value_site_cat$protectedarea>0)] <- 1
+value_site_cat$eulandsystem_cat <- NA
+value_site_cat$eulandsystem_cat[which(value_site_cat$eulandsystem %in% c(11,12,13,80,90))] <-"no_intensity"
+value_site_cat$eulandsystem_cat[which(value_site_cat$eulandsystem %in% c(21,31,41,51,61,731,71:75))] <-"low_intensity"
+value_site_cat$eulandsystem_cat[which(value_site_cat$eulandsystem %in% c(22,32,42,52,62,732))] <-"medium_intensity"
+value_site_cat$eulandsystem_cat[which(value_site_cat$eulandsystem %in% c(23,43,53,63,733))] <-"high_intensity"
+value_site_cat$eulandsystem_cat <- factor(value_site_cat$eulandsystem_cat, levels = c("no_intensity","low_intensity","medium_intensity","high_intensity")) 
+value_site_cat$eulandsystem <- NULL
+
+
+## add zero when species no present at monitored site
 
 wide_bird_data <- data.frame(bird_data_cat_all[,c("siteID","year","sci_name_out","count")] %>% group_by(siteID) %>% tidyr::complete(year,sci_name_out))
 wide_bird_data$count[which(is.na(wide_bird_data$count))] <- 0
@@ -111,11 +226,13 @@ bird_data_cat <- merge(wide_bird_data,site_cat_all,by=c("siteID"), all.x=T)
 
 bird_data_cat <- merge(bird_data_cat,value_site_cat, by="siteID", all.x=TRUE)
 
-# test with most commun species
+
+
+## test with the most commun species
 
 fricoe_data_cat <- droplevels(bird_data_cat[which(bird_data_cat$sci_name_out=="Fringilla coelebs"),])
 
-# compute trend species
+### compute trend species
 
 nb_year_p_site <- data.frame(fricoe_data_cat %>% group_by(siteID) %>% summarise(nb_year=n(),
                                                                                      min_year=min(year),
@@ -136,13 +253,13 @@ fricoe_cat_trend <- ddply(fricoe_data_cat_trend, .(siteID),
                             },
                           .progress = "text")
 
-# Multiscale gwr
+### prepare data for Multiscale gwr
 
 fricoe_cat_trend <- merge(fricoe_cat_trend,site_cat_all,by=c("siteID"), all.x=TRUE)
 fricoe_cat_trend <- merge(fricoe_cat_trend,value_site_cat, by="siteID", all.x=TRUE)
 
 fricoe_cat_data_analysis <- fricoe_cat_trend[,c("Estimate","mean_ab","pop2000","impervious2006","treedensity2012",
-                                                "eulandsystem","protectedarea","lightpollution2000",
+                                                "eulandsystem_cat","protectedarea","lightpollution2000",
                                                 "pesticide_nodu_kg","woodprod2000","drymatter2000",
                                                 "smallwoodyfeatures","fragmentation","forestintegrity_cat",
                                                 "tempspring2000","tempspringmaxvar2000","precspring2000",
@@ -150,16 +267,6 @@ fricoe_cat_data_analysis <- fricoe_cat_trend[,c("Estimate","mean_ab","pop2000","
                                                 "shannon",'scheme_code',
                                                 "Long_WGS84", "Lat_WGS84")]
 
-fricoe_cat_data_analysis$protectedarea[which(is.na(fricoe_cat_data_analysis$protectedarea))] <- 0
-fricoe_cat_data_analysis$protectedarea[which(fricoe_cat_data_analysis$protectedarea>0)] <- 1
-fricoe_cat_data_analysis <- na.omit(fricoe_cat_data_analysis)
-fricoe_cat_data_analysis$eulandsystem_cat <- NA
-fricoe_cat_data_analysis$eulandsystem_cat[which(fricoe_cat_data_analysis$eulandsystem %in% c(11,12,13,80,90))] <-"no_intensity"
-fricoe_cat_data_analysis$eulandsystem_cat[which(fricoe_cat_data_analysis$eulandsystem %in% c(21,31,41,51,61,731,71:75))] <-"low_intensity"
-fricoe_cat_data_analysis$eulandsystem_cat[which(fricoe_cat_data_analysis$eulandsystem %in% c(22,32,42,52,62,732))] <-"medium_intensity"
-fricoe_cat_data_analysis$eulandsystem_cat[which(fricoe_cat_data_analysis$eulandsystem %in% c(23,43,53,63,733))] <-"high_intensity"
-fricoe_cat_data_analysis$eulandsystem_cat <- factor(fricoe_cat_data_analysis$eulandsystem_cat, levels = c("no_intensity","low_intensity","medium_intensity","high_intensity")) 
-fricoe_cat_data_analysis$eulandsystem <- NULL
 
 fricoe_cat_data_analysis[,c("pop2000","impervious2006","treedensity2012","lightpollution2000",
                             "pesticide_nodu_kg","woodprod2000","drymatter2000",
@@ -173,13 +280,14 @@ fricoe_cat_data_analysis[,c("pop2000","impervious2006","treedensity2012","lightp
 
 fricoe_cat_trend_sp <- st_as_sf(fricoe_cat_data_analysis, coords = c("Long_WGS84", "Lat_WGS84"), crs = 4326) 
 
+### run MSGWR
 
 gw.ms <- gwr.multiscale(Estimate ~ mean_ab + pop2000 + impervious2006 + treedensity2012 +
                         eulandsystem_cat + protectedarea + lightpollution2000 +
                         pesticide_nodu_kg + woodprod2000 + drymatter2000 +
                         smallwoodyfeatures + fragmentation + forestintegrity_cat +
                         tempspring2000 + tempspringmaxvar2000 + precspring2000 +
-                        precspringvar2000 + humidity2000 + shannon, 
+                        precspringvar2000 + humidity2000 + shannon + scheme_code, 
                         data = as(fricoe_cat_trend_sp, "Spatial"),
                         adaptive = T, max.iterations = 1000,
                         predictor.centered = rep(TRUE,(ncol(fricoe_cat_data_analysis)-3)),
@@ -193,3 +301,108 @@ round(coefs_msgwr,1)
 mgwr_sf <- st_as_sf(gw.ms$SDF)
 tm_shape(mgwr_sf) + tm_bubbles("woodprod2000",col="woodprod2000")+
   tm_layout(legend.position = c("right","top"), frame = F)
+
+
+## expend trend analysis for all species
+
+### compute trend species
+
+sub_bird_data <- droplevels(bird_data_cat[which(bird_data_cat$sci_name_out=="Fringilla coelebs"),])
+
+
+nb_year_p_site <- data.frame(bird_data_cat %>% group_by(siteID, year) %>% summarise(count=n()))
+nb_year_p_site <- data.frame(nb_year_p_site %>% group_by(siteID) %>% summarise(nb_year=n(),
+                                                            min_year=min(year),
+                                                            max_year=max(year)))
+
+selected_site_trend <- nb_year_p_site[which(nb_year_p_site$nb_year > 4 &
+                                              nb_year_p_site$min_year < 2006 &
+                                              nb_year_p_site$max_year > 2014),]
+
+bird_data_cat_trend <- bird_data_cat[which(bird_data_cat$siteID %in% c(selected_site_trend$siteID)),]
+
+get_species_trend <- function(sub_bird_data){
+  mod <- glm(count~year, data=sub_bird_data, family = "poisson")
+  mod_result <- as.data.frame(t(summary(mod)$coef[2,]))
+  mod_result$mean_ab <- mean(sub_bird_data$count)
+  return(mod_result)
+}
+
+bird_cat_trend <- ddply(bird_data_cat_trend, .(sci_name_out, siteID),
+                        .fun = get_species_trend,
+                        .progress = "text")
+
+### remove outlier trends
+
+bird_cat_trend_clean <- bird_cat_trend[which(abs(bird_cat_trend$Estimate) > bird_cat_trend$`Std. Error`),]
+
+### prepare and format data for multiscale gwr
+
+bird_data_mgwr_cat <- merge(bird_cat_trend_clean,site_cat_all,by=c("siteID"), all.x=TRUE)
+bird_data_mgwr_cat <- merge(bird_data_mgwr_cat,value_site_cat, by="siteID", all.x=TRUE)
+
+bird_data_mgwr_cat <- bird_data_mgwr_cat[,c("sci_name_out","Estimate","mean_ab","pop2000","impervious2006","treedensity2012",
+                                                "eulandsystem_cat","protectedarea","lightpollution2000",
+                                                "pesticide_nodu_kg","woodprod2000","drymatter2000",
+                                                "smallwoodyfeatures","fragmentation","forestintegrity_cat",
+                                                "tempspring2000","tempspringmaxvar2000","precspring2000",
+                                                "precspringvar2000","humidity2000",
+                                                "shannon",'scheme_code',
+                                                "Long_WGS84", "Lat_WGS84")]
+
+
+
+bird_data_mgwr_cat[,c("pop2000","impervious2006","treedensity2012","lightpollution2000",
+                            "pesticide_nodu_kg","woodprod2000","drymatter2000",
+                            "smallwoodyfeatures","fragmentation",
+                            "tempspring2000","tempspringmaxvar2000","precspring2000",
+                            "precspringvar2000","humidity2000","shannon")] <- scale(bird_data_mgwr_cat[,c("pop2000","impervious2006","treedensity2012","lightpollution2000",
+                                                                                                                "pesticide_nodu_kg","woodprod2000","drymatter2000",
+                                                                                                                "smallwoodyfeatures","fragmentation",
+                                                                                                                "tempspring2000","tempspringmaxvar2000","precspring2000",
+                                                                                                                "precspringvar2000","humidity2000","shannon")])
+### run MGWR per species
+
+mgwr_bird <- function(sub_bird_data_mgwr){
+  
+  sub_bird_data_mgwr <- na.omit(sub_bird_data_mgwr)
+  
+  if(nrow(sub_bird_data_mgwr) > 40){ # 20 variables so a minimum of data is required
+    sub_bird_data_mgwr_sp <- st_as_sf(sub_bird_data_mgwr, coords = c("Long_WGS84", "Lat_WGS84"), crs = 4326) 
+    
+    gw.ms <- gwr.multiscale(Estimate ~ mean_ab + pop2000 + impervious2006 + treedensity2012 +
+                              eulandsystem_cat + protectedarea + lightpollution2000 +
+                              pesticide_nodu_kg + woodprod2000 + drymatter2000 +
+                              smallwoodyfeatures + fragmentation + forestintegrity_cat +
+                              tempspring2000 + tempspringmaxvar2000 + precspring2000 +
+                              precspringvar2000 + humidity2000 + shannon + scheme_code, 
+                            data = as(sub_bird_data_mgwr_sp, "Spatial"),
+                            adaptive = T, max.iterations = 1000,
+                            predictor.centered = rep(TRUE,(ncol(sub_bird_data_mgwr_sp)-3)),
+                            criterion="CVR",
+                            kernel = "bisquare",
+                            bws0 = rep(100,(ncol(sub_bird_data_mgwr_sp)-3)),
+                            verbose = F)
+    gw.ms$GW.arguments$bws
+    coefs_msgwr <- apply(gw.ms$SDF@data[, 1:(ncol(sub_bird_data_mgwr_sp))], 2, summary)
+    res_data <- data.frame(t(coefs_msgwr[3,]))
+  }else{
+    res_data <- data.frame(Intercept=NA,mean_ab=NA,pop2000=NA,impervious2006=NA,
+                       treedensity2012=NA,eulandsystem_catlow_intensity=NA,eulandsystem_catmedium_intensity=NA,eulandsystem_cathigh_intensity=NA,
+                       protectedarea=NA,lightpollution2000=NA,pesticide_nodu_kg=NA,woodprod2000=NA,
+                       drymatter2000=NA,smallwoodyfeatures=NA,fragmentation=NA,forestintegrity_catmedium=NA,
+                       tempspring2000=NA,tempspringmaxvar2000=NA,precspring2000=NA,precspringvar2000=NA,
+                       humidity2000=NA,shannon=NA,scheme_codeES_CAT=NA)
+    }
+  
+  return(res_data)
+}
+
+bird_cat_mgwr <- ddply(bird_data_mgwr_cat, .(sci_name_out),
+                        .fun = mgwr_bird,
+                        .progress = "text")
+
+test <- ddply(bird_data_mgwr_cat[which(bird_data_mgwr_cat$sci_name_out %in%
+                                         unique(bird_data_mgwr_cat$sci_name_out)[36]),], .(sci_name_out),
+              .fun = mgwr_bird,
+              .progress = "text")
